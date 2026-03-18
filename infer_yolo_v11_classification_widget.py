3f5dd46fbe4c5fd1dfe2ccbb2f55482b7fb435ed
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2026-03-18 15:22:10 +0100

    chore: Qt 6

--- infer_yolo_v11_classification_widget.py
+++ infer_yolo_v11_classification_widget.py
@@ -1,10 +1,12 @@
+# PyQt GUI framework
+from PyQt6.QtWidgets import *
+
+from torch.cuda import is_available
+
 from ikomia import core, dataprocess
 from ikomia.utils import pyqtutils, qtconversion
-from infer_yolo_v11_classification.infer_yolo_v11_classification_process import InferYoloV11ClassificationParam
 
-# PyQt GUI framework
-from PyQt5.QtWidgets import *
-from torch.cuda import is_available
+from infer_yolo_v11_classification.infer_yolo_v11_classification_process import InferYoloV11ClassificationParam
 
 
 # --------------------
@@ -52,7 +54,7 @@ class InferYoloV11ClassificationWidget(core.CWorkflowTaskWidget):
         self.browse_weight_file = pyqtutils.BrowseFileWidget(
                                         path=self.parameters.model_weight_file,
                                         tooltip="Select file",
-                                        mode=QFileDialog.ExistingFile
+                                        mode=QFileDialog.FileMode.ExistingFile
         )
         row = self.grid_layout.rowCount()
         self.grid_layout.addWidget(self.label_hyp, row, 0)
